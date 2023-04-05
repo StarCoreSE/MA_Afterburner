50fe58e6e725929dfe53f3981c2a3e0f6f0680c3
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-04-05 16:59:27 -0500

    test1

--- Data/Scripts/SRBanticringe/SRBanticringeexplode.cs
+++ Data/Scripts/SRBanticringe/SRBanticringeexplode.cs
@@ -1,15 +1,14 @@
-using System;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Game;
 using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Blocks;
 using Sandbox.ModAPI;
-using VRage.Game;
+using System;
 using VRage.Game.Components;
-using VRage.Game.ModAPI;
-using VRage.Game.VisualScripting;
 using VRage.ModAPI;
 using VRage.ObjectBuilders;
 using VRageMath;
+
 
 namespace SRBanticringe
 {
@@ -44,20 +43,55 @@ namespace SRBanticringe
 
             if (!SRB.Enabled)
             {
-				// Turn on the block
+                // Turn on the block
 
-				//SRB.Enabled = true; 
-				//MyAPIGateway.Utilities.ShowNotification("The solid fuel is still burning.", 10000, "Green");
+                //SRB.Enabled = true; 
+                //MyAPIGateway.Utilities.ShowNotification("The solid fuel is still burning.", 10000, "Green");
 
-				//Or it could just fucking explode
+                //Or it could just fucking explode
 
-				//MyAPIGateway.Utilities.ShowNotification("An SRB has detonated from overpressure.", 10000, "Red");
-                Vector3D position = Entity.GetPosition();
-                Sandbox.Game.MyVisualScriptLogicProvider.CreateExplosion(position, 50f, 100000);
+                //MyAPIGateway.Utilities.ShowNotification("An SRB has detonated from overpressure.", 10000, "Red");
+
+                /*  Vector3D myPos = Entity.GetPosition();
+                  float dmg = 10000f;
+                  float radius = 100f;
+
+                
+                  //Explosion Damage!
+                  BoundingSphereD sphere = new BoundingSphereD(myPos, radius);
+                  MyExplosionInfo bomb = new MyExplosionInfo(dmg, dmg, sphere, MyExplosionTypeEnum.MISSILE_EXPLOSION, true, true);
+                  bomb.CreateParticleEffect = false;
+                  bomb.LifespanMiliseconds = 150 + (int)radius * 45;
+                  MyExplosions.AddExplosion(ref bomb, true);
+                */
+
+
+                Vector3D myPos = Entity.GetPosition();
+                double radius = 100;
+                BoundingSphereD explosionSphere = new BoundingSphereD(myPos, radius);
+                MyExplosionInfo explosionInfo = new MyExplosionInfo()
+                {
+                    PlayerDamage = 0f,
+                    Damage = 10f,
+                    ExplosionType = MyExplosionTypeEnum.GRID_DESTRUCTION,
+                    ExplosionSphere = explosionSphere,
+                    LifespanMiliseconds = 700,
+                    ParticleScale = 1f,
+                    OwnerEntity = null, 
+                    ExplosionFlags = MyExplosionFlags.AFFECT_VOXELS | MyExplosionFlags.APPLY_FORCE_AND_DAMAGE | MyExplosionFlags.CREATE_DECALS | MyExplosionFlags.CREATE_PARTICLE_EFFECT | MyExplosionFlags.CREATE_SHRAPNELS | MyExplosionFlags.APPLY_DEFORMATION | MyExplosionFlags.CREATE_PARTICLE_DEBRIS,
+                    VoxelCutoutScale = 1f,
+                    PlaySound = true,
+                    ApplyForceAndDamage = true,
+                    KeepAffectedBlocks = false,
+                    StrengthImpulse = 0.0f
+                };
+
+                MyExplosions.AddExplosion(ref explosionInfo);
+
 
 
 
             }
-		}
+        }
     }
 }
